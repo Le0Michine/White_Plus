5b0f05f8baaeba796f1e262af25ec6fafb9a88c2
Maria Elfimova <mirandablackheart@gmail.com>
2013-12-03 13:27:12 +0400

    add new files

---
+++ WhiteTests/Tests/MathServiceTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using White.Core.UIItems.WindowItems;
+using ViewAccessors;
+using MathService;
+
+namespace MathServiceTest
+{
+    [TestClass]
+    public class MathServiceTest
+    {
+        Window window;
+        AccessorBase accessor;
+        Test test;
+        [TestInitialize]
+        public void Initialize()
+        {
+            accessor = new AccessorBase();
+            window = accessor.window;
+            test = new Test();
+        }
+
+        [TestMethod]
+        public void IdenticTest()
+        {
+            string resultString = "Representation for 6 includes: \n2\n3\n";
+            string resString = test.CalculationPhiFunc(window, "6");
+            Assert.AreEqual(resString, resultString);
+        }
+    }
+}
---
+++ WhiteTests/ViewAccessors/ViewAccessorBase.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using White.Core;
+using White.Core.Factory;
+using White.Core.UIItems.WindowItems;
+
+namespace ViewAccessors
+{
+    public class AccessorBase
+    {
+        private static string ExeSourceFile = @"D:\study\White+\TestApps\Cannonical representation for number\Cannonical representation for number\bin\Debug\Cannonical representation for number.exe";
+        private static Application application;
+        public  Window window;
+        
+        /// <summary>
+        /// to do: window 
+        /// </summary>
+        public AccessorBase() 
+        {
+            application = Application.Launch(ExeSourceFile);
+            window = application.GetWindow("Decomposition of Number", InitializeOption.NoCache);
+        }
+    }
+}
